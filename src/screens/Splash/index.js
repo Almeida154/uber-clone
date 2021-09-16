0f44b8768b8afeca5b40ff87a032b6acb0e8c8e0
Almeida154 <davidalmeida154of@gmail.com>
2021-09-16 14:16:46 -0300

    :sparkles: TouchId working

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import TouchID from 'react-native-touch-id';
 
-import { StatusBar } from 'react-native';
+import { StatusBar, Text } from 'react-native';
 import { Container, Title } from './styles';
 import { colors } from '../../styles';
 import Config from '../../config';
 
 export default function Splash({ navigation }) {
 
-    const handleSplashTime = () => {
-        setTimeout(() => {
-            navigation.navigate('Home');
-        }, Config.SPLASH_TIMEOUT);
+    const [isSupported, setSupported] = useState(null);
+
+    useEffect(() => {
+        TouchID.isSupported()
+            .then(success => {
+                setSupported(true);
+            })
+            .catch(error => {
+                console.log('ERRO NO TOUCHID: ' + error);
+                alert('nao suportado');
+            });
+    }, []);
+
+    const handleAuth = () => {
+        TouchID.authenticate('Faça a autenticação para entrar', {
+            title: 'Autenticação',
+            imageColor: colors.black,
+            sensorErrorDescription: 'Deu errado! :/',
+            cancelText: 'Cancelar'
+        })
+            .then(success => navigation.navigate('Home'))
+            .catch(error => console.log('Falha: ' + error))
     }
 
+    const handleSplashTime = () => {
+        setTimeout(() => {
+            isSupported ? handleAuth() : null;
+        }, Config.SPLASH_TIMEOUT);
+    }
+    
     return (
         <Container>
             <StatusBar backgroundColor={colors.black} />
             <Title>Uber</Title>
+            { !isSupported && <Text>Seu celular não suporta algumas funcionalidades.</Text> }
             { handleSplashTime() }
         </Container>
     );
